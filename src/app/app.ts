9bfc3aa7c21ca3327f254ffbf2f289b8b1f6132a
Kacper Czaja <kacperczaja@duck.com>
2025-09-26 22:00:24 +0200

    The first three buttons with class change

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,31 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgClass, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgClass, NgFor],
   template: `
-    <h1>Welcome to {{ title() }}!</h1>
-
-    <router-outlet />
+    <h1>Welcome to {{ title().length == 0 ? 'Dodaj tytuł strony' : title() }}!</h1>
+    <ul>
+      <li *ngFor="let task of tasks; let i = index">
+        <button [ngClass]="task.isActive ? 'btn-active' : 'btn-default'" (click)="toggleButton(i)">
+          {{ task.title }}
+        </button>
+      </li>
+    </ul>
   `,
-  styles: [],
+  //  styles: [],
 })
 export class App {
-  protected readonly title = signal('pierwszy-poradnik');
+  protected readonly title = signal('Pierwsza aplikacja napisana w Angularze');
+
+  protected readonly tasks = [
+    { title: 'Kliknij mnie', isActive: true },
+    { title: 'Kliknij mnie 2', isActive: false },
+    { title: 'Kliknij mnie 3', isActive: true },
+  ];
+
+  toggleButton(taskNumber: number): void {
+    this.tasks[taskNumber].isActive = !this.tasks[taskNumber].isActive;
+  }
 }
